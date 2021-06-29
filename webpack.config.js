f5ebf0bf648f16652195d8d1e28c2831bd098ebe
Mai Houa Vue <maihoua.vue@mvue-mbp.local>
2021-06-29 08:27:30 -0500

    setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,70 @@
 //entry point 
 //where to output bundle file
 const path = require("path");
+const MiniCssExtractPlugin= require("mini-css-extract-plugin");
 
-console.log(path.join(__dirname, "public"));
+module.exports = (env) => {
+    const isProduction = env === "production";
+    const CSSExtract = new MiniCssExtractPlugin({ filename: "styles.css" })
 
-module.exports = { 
-    mode: "development",
-    entry: "./src/app.js",
-    output: {
-        path: path.join(__dirname, "public"),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [{
-            loader: "babel-loader",
-            test: /\.js$/,
-            exclude: /node_modules/,
-            options: {
-            presets: [
-
-                ['@babel/preset-env', 
-                    { 
-                        'useBuiltIns': 'entry',
-                        'corejs': '^3.12.1',
-                        /* 'targets': {
-                                    'esmodules': true,
-                                    'ie': '11'
-                                    } */
-                    }
+    console.log("env", env)
+    return { 
+        entry: "./src/app.js",
+        output: {
+            path: path.join(__dirname, "public"),
+            filename: "bundle.js"
+        },
+        module: {
+            rules: [{
+                loader: "babel-loader",
+                test: /\.js$/,
+                exclude: /node_modules/,
+                options: {
+                presets: [
+    
+                    ['@babel/preset-env', 
+                        { 
+                            'useBuiltIns': 'entry',
+                            'corejs': '^3.12.1',
+                            /* 'targets': {
+                                        'esmodules': true,
+                                        'ie': '11'
+                                        } */
+                        }
+                    ],
+                    '@babel/preset-react',
                 ],
-                '@babel/preset-react',
-            ],
-            plugins: ['babel-plugin-transform-class-properties']
-
+                plugins: ['babel-plugin-transform-class-properties']
+    
+            }
+            }, {
+                test: /\.s?css$/,
+                use: [MiniCssExtractPlugin.loader, 
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    },  {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    }]
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? "source-map":  "inline-cheap-module-source-map",
+        devServer: {
+            contentBase: path.join(__dirname, "public"),
+            historyApiFallback: true
         }
-        }, {
-            test: /\.s?css$/,
-            use: [
-                "style-loader",
-                "css-loader",
-                "sass-loader",
-            ]
-        }]
-    },
-
-    devtool: "eval-cheap-module-source-map",
-    devServer: {
-        contentBase: path.join(__dirname, "public"),
-        historyApiFallback: true
+    
     }
-
 }
+
+
 
 //loader - let u customize a file when it loads 
